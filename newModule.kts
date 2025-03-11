8f24c0e259135dd3c926cd6be67b4dd62cd9c5cb
Bogdanov Mikhail <mikhail.bogdanov.w@gmail.com>
2025-03-11 22:36:59 +0300

    Refactor maneModule script

--- newModule.kts
+++ newModule.kts
@@ -1,125 +1,77 @@
 import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.createDirectories
+import kotlin.io.path.pathString
+import kotlin.io.path.Path as KPath
 
-/*===========================*/
-val MODULE_NAME = "AAA"
-val PACKAGE_NAME = "AAA"
-val SCREEN_NAME = "AAA"
-/*===========================*/
+val MODULE_NAME = "settings-screen" // exaple: main-page
 
-val sep = File.separator
+//////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-val featurePath = File("feature").absolutePath
-val newModule = File(featurePath.plus(sep).plus(MODULE_NAME))
-
-val namespace = PACKAGE_NAME.lowercase()
+val SCREEN_NAME = MODULE_NAME.toScreenName()
+val module = KPath(File("feature").absolutePath, MODULE_NAME)
+val namespace = SCREEN_NAME.lowercase()
 val screenNameImpl = "${SCREEN_NAME}Impl"
 
-
-val apiNewModule = File(newModule.absolutePath.plus(sep).plus("api"))
-apiNewModule.mkdirs()
-
-val apiPath = buildList {
-    add(apiNewModule.absolutePath)
-    add("src")
-    add("main")
-    add("kotlin")
-    add("com")
-    add("evo")
-    add(namespace)
-    add("ui")
-}.joinToString(sep)
-
-val newModuleApiDir = File(apiPath)
-newModuleApiDir.mkdirs()
-
-val newModuleApiScreenFile = File(
-    newModuleApiDir.absolutePath.plus(sep).plus("$SCREEN_NAME.kt")
-)
-newModuleApiScreenFile.createNewFile()
-
-val funName = SCREEN_NAME.replaceFirstChar { it.lowercase() }
-
-newModuleApiScreenFile.writeText(
-    """
-        package com.evo.$namespace.ui
-
-        import androidx.compose.runtime.Composable
-        import com.evo.navigation.Screen
-        import org.koin.compose.koinInject
-
-        interface $SCREEN_NAME : Screen
-
-        @Composable
-        fun $funName() = koinInject<$SCREEN_NAME>()
-    """.trimIndent()
-)
-
-val newModuleApiBuildGradleFile = File(
-    apiNewModule.absolutePath.plus(sep).plus("build.gradle.kts")
-).apply { createNewFile() }
-
-newModuleApiBuildGradleFile.writeText(
+val moduleApiDir = KPath(module.absolutePath, "api").mkdirs()
+val apiPath = createFullModulePath(moduleApiDir, namespace, KPath("ui"))
+makeFile(moduleApiDir, "build.gradle.kts") {
     """
         plugins {
             id("evo-compose")
         }
 
         android.namespace = "com.evo.$namespace.api"
-    """.trimIndent()
-)
+    """
+}
+makeFile(apiPath, "$SCREEN_NAME.kt") {
+    """
+        package com.evo.$namespace.ui
+    
+        import androidx.compose.runtime.Composable
+        import com.evo.navigation.Screen
+        import org.koin.compose.koinInject
+    
+        interface $SCREEN_NAME : Screen
+    
+        @Composable
+        fun ${SCREEN_NAME.lowercaseFirst()}() = koinInject<$SCREEN_NAME>()
+    """
+}
 
-val implNewModule = File(newModule.absolutePath.plus(sep).plus("impl"))
-implNewModule.mkdirs()
-
-val implPath = buildList {
-    add(implNewModule.absolutePath)
-    add("src")
-    add("main")
-    add("kotlin")
-    add("com")
-    add("evo")
-    add(namespace)
-}.joinToString(sep)
-
-val newModuleImplDir = File(implPath)
-newModuleImplDir.mkdirs()
-
-val newModuleImplScreenDir = File(implPath.plus(sep).plus("ui"))
-newModuleImplScreenDir.mkdirs()
-
-val newModuleImplScreenFile = File(
-    newModuleImplScreenDir.absolutePath.plus(sep).plus("$SCREEN_NAME.kt")
-)
-
-newModuleImplScreenFile.createNewFile()
-
-val newModuleImplDiDir = File(implPath.plus(sep).plus("di"))
-newModuleImplDiDir.mkdirs()
-
-val newModuleImplDiModuleFile = File(
-    newModuleImplDiDir.absolutePath.plus(sep).plus("${SCREEN_NAME}Module.kt")
-)
-
-newModuleImplDiModuleFile.createNewFile()
-
-newModuleImplScreenFile.writeText(
+val moduleImplDir = KPath(module.absolutePath, "impl").mkdirs()
+val implPath = createFullModulePath(moduleImplDir, namespace)
+makeFile(moduleImplDir, "build.gradle.kts") {
+    """
+        plugins {
+            id("feature")
+        }
+        
+        android.namespace = "com.evo.$namespace.impl"
+        
+        dependencies {
+            implementation(projects.feature.${SCREEN_NAME.lowercaseFirst()}.api)
+        }
+    """
+}
+makeFile(implPath, KPath("ui"), "$SCREEN_NAME.kt") {
     """
         package com.evo.$namespace.ui
 
         import androidx.compose.runtime.Composable
         import com.evo.navigation.EvoNavigationHandler
-        
+    
         internal class $screenNameImpl : $SCREEN_NAME {
-        
+    
             @Composable
             override fun Content(navigator: EvoNavigationHandler) {
-        
+    
             }
         }
-    """.trimIndent()
-)
-
-newModuleImplDiModuleFile.writeText(
+    """
+}
+makeFile(implPath, KPath("di"), "${SCREEN_NAME}Module.kt") {
     """
         package com.evo.$namespace.di
 
@@ -136,25 +88,62 @@ newModuleImplDiModuleFile.writeText(
         
             operator fun invoke() = module
         }
-    """.trimIndent()
-)
-
-val newModuleImplBuildGradleFile = File(
-    implNewModule.absolutePath.plus(sep).plus("build.gradle.kts")
-)
-
-newModuleImplBuildGradleFile.createNewFile()
-
-newModuleImplBuildGradleFile.writeText(
     """
-        plugins {
-            id("feature")
-        }
-        
-        android.namespace = "com.evo.$namespace.impl"
-        
-        dependencies {
-            implementation(projects.feature.$PACKAGE_NAME.api)
-        }
-    """.trimIndent()
-)
+}
+
+fun String.lowercaseFirst() = this.replaceFirstChar { it.lowercase() }
+fun String.uppercaseFirst() = this.replaceFirstChar { it.uppercase() }
+fun String.toScreenName(): String {
+    var tempModuleName = toMutableList()
+
+    val isValidName = all { letter ->
+        letter in 'A'..'z' || letter == '-'
+    } && startsWith('-').not() && endsWith('-').not()
+
+    check(isValidName)
+
+    while (tempModuleName.contains('-')) {
+        val hyphenIndex = tempModuleName.indexOf('-')
+        val hyphenNextSymbol = tempModuleName[hyphenIndex + 1]
+        tempModuleName.set(hyphenIndex + 1, hyphenNextSymbol.uppercaseChar())
+        tempModuleName.removeAt(hyphenIndex)
+    }
+
+    return tempModuleName.joinToString("").uppercaseFirst()
+}
+
+fun Path.createFile() = this.toFile().apply { createNewFile() }
+fun Path.mkdirs() = this.apply { createDirectories() }
+val Path.absolutePath get() = this.absolutePathString()
+
+fun createFullModulePath(modulePath: Path, namespace: String, extraDirs: Path = KPath("")) = KPath(
+    modulePath.absolutePath,
+    "src",
+    "main",
+    "kotlin",
+    "com",
+    "evo",
+    namespace,
+    extraDirs.pathString,
+).mkdirs()
+
+fun makeFile(path: Path, name: String) = KPath(path.absolutePath, name).createFile()
+
+fun makeFile(
+    path: Path,
+    extraDirs: Path = KPath(""),
+    name: String,
+) = makeFile(KPath(path.absolutePath, extraDirs.pathString).mkdirs(), name)
+
+fun makeFile(
+    path: Path,
+    name: String,
+    content: () -> String
+) = this.makeFile(path, name).writeText(content().trimIndent())
+
+fun makeFile(
+    path: Path,
+    extraDirs: Path = KPath(""),
+    name: String,
+    content: () -> String
+) = this.makeFile(path, extraDirs, name).writeText(content().trimIndent())
