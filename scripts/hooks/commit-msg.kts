aaacd60a7014cf8377a28a6cd5d0e1eb24959ef2
Bogdanov Mikhail <mikhail.bogdanov.w@gmail.com>
2025-06-24 21:49:54 +0300

    update gih hook

--- scripts/hooks/commit-msg.kts
+++ scripts/hooks/commit-msg.kts
@@ -1,19 +1,33 @@
 import java.io.File
+import java.util.Properties
 
-if (args.isEmpty()) {
-    println("❌ Commit message file path not provided.")
+try {
+    val commitMsgFile = File(args[0])
+    val originalMessage = commitMsgFile.readText().trim()
+
+    val propertiesFile = File("gradle.properties")
+    val properties = Properties().apply {
+        load(propertiesFile.inputStream())
+    }
+
+    val MAJOR_CODE = properties.getProperty("MAJOR_CODE").toInt() + 1
+    val MINOR_CODE = properties.getProperty("MINOR_CODE").toInt() + 1
+    val PATCH_CODE = properties.getProperty("PATCH_CODE").toInt() + 1
+
+    properties.setProperty("PATCH_CODE", PATCH_CODE.toString())
+
+    propertiesFile.outputStream().use { properties.store(it, null) }
+
+    val process = ProcessBuilder("git", "add", "gradle.properties")
+        .inheritIO()
+        .start()
+    process.waitFor()
+
+//    throw Exception("asd")
+
+    commitMsgFile.writeText(originalMessage)
+
+} catch (e: Exception) {
+    println(e.localizedMessage)
     kotlin.system.exitProcess(1)
 }
-
-val commitMsgFile = File(args[0])
-if (!commitMsgFile.exists()) {
-    println("❌ Commit message file not found: ${args[0]}")
-    kotlin.system.exitProcess(1)
-}
-
-val original = commitMsgFile.readText().trim()
-val updated = "[task] $original"
-
-commitMsgFile.writeText(updated)
-
-println("✅ Commit message updated to: $updated")
