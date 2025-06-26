0184f67e9fddf4dacabdc5f4092636ba419518a1
Bogdanov Mikhail <mikhail.bogdanov.w@gmail.com>
2025-06-26 15:20:25 +0300

    [PATCH] Bumped from 8 to 9
    Update sample features

--- feature/log-in/impl/src/main/kotlin/com/evo/login/LogIn.kt
+++ feature/log-in/impl/src/main/kotlin/com/evo/login/LogIn.kt
@@ -1,13 +1,15 @@
 package com.evo.login
 
 import androidx.compose.runtime.Composable
-import com.evo.screen.NoArgs
 import com.evo.screen.Screen
+import org.koin.core.component.inject
 
-internal class LogIn : Screen<NoArgs, ScreenModel>(NoArgs, ScreenModel::class) {
+internal class LogIn : Screen<ScreenModel>() {
+
+    override val screenModel: ScreenModel by inject()
 
     @Composable
     override fun Content() {
-        
+
     }
 }
--- feature/sign-up/impl/src/main/kotlin/com/evo/signup/SignUp.kt
+++ feature/sign-up/impl/src/main/kotlin/com/evo/signup/SignUp.kt
@@ -1,13 +1,15 @@
 package com.evo.signup
 
 import androidx.compose.runtime.Composable
-import com.evo.screen.NoArgs
 import com.evo.screen.Screen
+import org.koin.core.component.inject
 
-internal class SignUp : Screen<NoArgs, ScreenModel>(NoArgs, ScreenModel::class) {
+internal class SignUp : Screen<ScreenModel>() {
+
+    override val screenModel: ScreenModel by inject()
 
     @Composable
     override fun Content() {
-        
+
     }
 }
